64793964cc43c78cd3da04dbc53dc1ce0370018a
mayakiindu-lab <mayakiindu@gmail.com>
2025-11-09 17:16:13 +0300

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,7 @@
+temp=int(input("Please enter Temperature: "))
+if temp >25:
+    print("It is hot. Stay hydrated.")
+elif temp >15 and temp < 25:
+    print("The weather is pleasant, enjoy your day.")
+else:
+    print("It is cold, Wear a jacket.")
