3e6726bdc8e7ef27ec883aba9548b6d63279a5ae
mayakiindu-lab <mayakiindu@gmail.com>
2025-11-09 17:15:18 +0300

    Add files via upload

---
+++ meal.py
@@ -0,0 +1,18 @@
+name=input("Enter Name: ")
+adm=int(input("Enter Admission Number: "))
+meal=input("Enter Meal Type: (Breakfast, Lunch, Supper)")
+amount=int(input("Enter Payment Amount: "))
+Balance=1000
+breakfast=100
+lunch=200
+supper=250
+
+
+
+print(name)
+print(adm)
+print(meal)
+print(amount)
+print('Transaction')
+
+
